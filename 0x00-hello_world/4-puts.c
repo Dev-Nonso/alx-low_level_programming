3276af857875952dcf6b87b7e8d7f318ad055493
Dev-Nonso <nonsoogbonna324@gmail.com>
2023-08-10 11:27:01 -0700

    function puts for printing in new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Printing files
+ *
+ * Return: return 0 for (successful)
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
