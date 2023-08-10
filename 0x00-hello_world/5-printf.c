ce57cda2ca47d2323f23d510489409775c5b6573
Dev-Nonso <nonsoogbonna324@gmail.com>
2023-08-10 12:36:32 -0700

    printing with printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Printing with printf func.
+ *
+ * Return: return 0 for successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
